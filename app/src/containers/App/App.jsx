fadb22ea772200112d41b78d144220a77518a5df
Anas Aboureada <anas.ieee@gmail.com>
2018-03-25 13:42:42 +0200

    Added App container

---
+++ app/src/containers/App/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import Header from './../../components/Header/Header';
+import CarsContainer from './../Cars/CarsContainer';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Header />
+    <CarsContainer />
+    <ToastContainer />
+  </div>
+);
+
+export default App;
