77d6e44d5ef23a42b25a0211de2802d2bce6e77c
Anas Aboureada <anas.ieee@gmail.com>
2018-03-25 13:41:40 +0200

    Added Header component

---
+++ app/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <header>
+    <img className="logo" src="./img/volvo-logo-scaled.png" alt="Volvo Logo" />
+  </header>
+);
+
+export default Header;
